cb330877c4b46fd1942f08507016c72d192f7031
TayunCodes <tayunparkdev@gmail.com>
2024-11-13 10:59:20 -0500

    thumbsup, thumbsdown, delete features added

--- README.md
+++ README.md
@@ -1,13 +1,6 @@
-## Installation
+## Basecode
+Savage-Auth : https://github.com/leonnoel/savage-auth
 
-1. Clone repo
-2. run `npm install`
-
-## Usage
-
-1. run `node server.js`
-2. Navigate to `localhost:8080`
-
-## Credit
-
-Modified from Scotch.io's auth tutorial
+## Features
+1. Thumbsup and thumbsdown counter working and updating
+2. Added delete function
--- app/routes.js
+++ app/routes.js
@@ -29,18 +29,18 @@ module.exports = function(app, passport, db) {
 // message board routes ===============================================================
 
     app.post('/messages', (req, res) => {
-      db.collection('messages').save({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
+      db.collection('messages').save({name: req.body.name, msg: req.body.msg, likes: 0}, (err, result) => {
         if (err) return console.log(err)
         console.log('saved to database')
         res.redirect('/profile')
       })
     })
 
-    app.put('/messages', (req, res) => {
+    app.put('/addOneLike', (req, res) => {
       db.collection('messages')
       .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
         $set: {
-          thumbUp:req.body.thumbUp + 1
+          likes:req.body.thumbUp + 1
         }
       }, {
         sort: {_id: -1},
@@ -51,12 +51,38 @@ module.exports = function(app, passport, db) {
       })
     })
 
-    app.delete('/messages', (req, res) => {
+    app.put('/removeOneLike', (req, res) => {
+      db.collection('messages')
+      .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+        $set: {
+          likes:req.body.thumbDown - 1
+        }
+      }, {
+        sort: {_id: -1},
+        upsert: true
+      }, (err, result) => {
+        if (err) return res.send(err)
+        res.send(result)
+      })
+    })
+
+    app.delete('/delete', (req, res) => {
       db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
         if (err) return res.send(500, err)
         res.send('Message deleted!')
       })
     })
+
+    // app.delete('/delete', (req, response) => {
+    //   db.collection('messages').deleteOne({ name: req.body.name, msg: req.body.msg})
+    //       .then(result => {
+    //         console.log(req.body.name, req.body.msg)
+
+    //           console.log('Deleted')
+    //           response.json('Deleted')
+    //       })
+    //       .catch(error => console.error(error))
+    // })
 
 // =============================================================================
 // AUTHENTICATE (FIRST LOGIN) ==================================================
